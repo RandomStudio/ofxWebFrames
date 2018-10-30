0628ecebd7551aae67218def0713b0de84422c70
Stephen <stephen.buchanan@random.studio>
2018-10-30 09:47:40 +0100

    works to fle output but still not piped for request

--- server/server.js
+++ server/server.js
@@ -1,16 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log('started server OK');
+const fs = require('fs');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function() {
+let imageData = null;
+
+process.stdin.on('readable', () => {
   var chunk = process.stdin.read();
   if (chunk !== null) {
-      console.log('chunk incoming:', chunk.toString());
+    console.log('chunk incoming:', chunk.toString());
+    // imageData += chunk;
+    let data = new Buffer(chunk, 'base64');
+    if (imageData === null) {
+        imageData = data;
+    } else {
+        imageData = Buffer.concat([imageData, data]);
+    }
+    // fs.appendFile('output.png', data, err => {
+    //     if (err) { throw new Error('error appending to file: ' + err); }
+    // });
+    console.log('imageData legnth:', imageData.length);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.exit();
 });
 
+
+app.get('/img', (req, res) => {
+    // const data = new Buffer(imageData, 'base64');
+    res.set("Content-Type", "image/png");
+    res.end(imageData, 'binary'); 
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+console.log('started server OK');
